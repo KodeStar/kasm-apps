1ebcc2edd5fc570b78f6a970aefb9e447387b43f
Chris Hunt <chris.hunt@kasmweb.com>
2022-11-01 11:33:09 +0000

    Changes to tile

--- site/components/App.js
+++ site/components/App.js
@@ -1,14 +1,14 @@
 function App({ Component, pageProps, app }) {
     return (
-        <div className="rectangular tile transition-all relative cursor-pointer group flex p-2 items-center justify-center bg-slate-100/90 dark:bg-slate-900/90 shadow rounded hover:shadow-xl hover:bg-gradient-to-r hover:from-slate-600 hover:to-blue-500">
+        <div className="w-[245px] h-[88px] transition-all relative cursor-pointer group flex p-2 items-center justify-center bg-slate-100/90 dark:bg-slate-900/90 shadow rounded hover:shadow-xl hover:bg-gradient-to-r hover:from-slate-600 hover:to-blue-500">
             <div className="w-full h-full">
                 <div className="show-grid flex h-full items-center">
-                    <div className="kasmcard-img flex h-full w-20 mx-4 items-center">
-                        <img className=" " src={ 'icons/' + app.icon} />
+                    <div className="kasmcard-img flex h-full mx-4 items-center justify-center">
+                        <img className="w-[50px] max-h-[66px]" src={ 'icons/' + app.icon} />
                     </div>
                     <div className="kasmcard-detail settingPad">
-                        <h5 className="">{ app.name }</h5>
-                        <p>{ app.categories[0] }</p>
+                        <h5 className="text-base">{ app.name }</h5>
+                        <p className="text-xs opacity-50">{ app.categories[0] }</p>
                     </div>
                 </div>
             </div>
