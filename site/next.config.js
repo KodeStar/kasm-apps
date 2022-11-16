198b7fc0b8205762da9173cf2d43a979c4330551
KodeStar <kodestar@gmail.com>
2022-11-16 15:59:51 +0000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  env: {
+    name: 'Kasm Apps',
+    description: 'The official store for Kasm supported applications.',
+    listUrl: 'https://apps.kasmweb.com/list.json',
+    contactUrl: 'https://kasmweb.com/support',
+  },
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/kasm-apps',
